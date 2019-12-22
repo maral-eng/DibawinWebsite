c8469b56a020ea7288d3dee930bb1e8b3f744007
H.Sadri <hossein.sadri@outlook.com>
2019-12-22 04:06:42 +0330

    blog-about-contact-ui-sadri

---
+++ DibawinWebsite/Controllers/PageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DibawinWebsite.Controllers
+{
+    public class PageController : Controller
+    {
+        #region Index
+        public IActionResult Index()
+        {
+            return RedirectToAction("About");
+        }
+        #endregion
+        #region About
+        public IActionResult About()
+        {
+            return View();
+        }
+        #endregion
+        #region ContactUs
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        #endregion
+
+    }
+}
